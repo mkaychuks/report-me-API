240f05cc51097ef48320c252a7dc309b4eadc46e
mkaychuks <sachufusi@gmail.com>
2020-03-10 10:06:15 +0100

    Heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn locallibrary.wsgi --log-file -
+web: gunicorn apps.wsgi --log-file -
--- apps/settings.py
+++ apps/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yis%gl66t#qv3ztic1eb&eu*b!bkf^
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reportmeapi.herokuapp.com']
 
 
 # Application definition
@@ -168,3 +168,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+import dj_database_url 
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
